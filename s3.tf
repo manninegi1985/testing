94ae4a2c2aeb8df7f6bcb9bd169df448be99adc4
manninegi1985 <negithecool@gmail.com>
2025-10-11 17:00:30 +0800

    remove S3

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket" "tf_cloud" {
-  bucket = "terraform-cloud-${random_id.this.hex}"
-  tags = {
-    CreatedBy = "AP-TEST"
-  }
-}
+#resource "aws_s3_bucket" "tf_cloud" {
+ # bucket = "terraform-cloud-${random_id.this.hex}"
+  #tags = {
+  #  CreatedBy = "AP-TEST"
+  #}
+#}
